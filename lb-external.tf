210d995d23ab391ccf451ccc52ed5e5dd19ee86b
An Nguyen <nthienan.it@gmail.com>
2023-11-12 10:19:34 +0700

    Update logic to create route table for public LB subnets

--- lb-external.tf
+++ lb-external.tf
@@ -1,19 +1,19 @@
 locals {
-  lb_external_subnets        = [for suffix in local.cidr_configs.lb_external : format("%s.%s", var.cidr_prefix, suffix)]
-  len_lb_external_subnets    = length(local.lb_external_subnets)
-  create_lb_external_subnets = local.create_vpc && var.create_lb_external_subnets && local.len_lb_external_subnets > 0
+  lb_external_subnets            = [for suffix in local.cidr_configs.lb_external : format("%s.%s", var.cidr_prefix, suffix)]
+  len_lb_external_subnets        = length(local.lb_external_subnets)
+  create_lb_external_subnets     = local.create_vpc && var.create_lb_external_subnets && local.len_lb_external_subnets > 0
   create_lb_external_route_table = local.create_lb_external_subnets && var.create_lb_external_subnet_route_table
 }
 
 resource "aws_subnet" "lb_external" {
   count = local.create_lb_external_subnets && (!var.one_nat_gateway_per_az || local.len_lb_external_subnets >= length(local.azs)) ? local.len_lb_external_subnets : 0
 
-  availability_zone                              = length(regexall("^[a-z]{2}-", element(local.azs, count.index))) > 0 ? element(local.azs, count.index) : null
-  availability_zone_id                           = length(regexall("^[a-z]{2}-", element(local.azs, count.index))) == 0 ? element(local.azs, count.index) : null
-  cidr_block                                     = element(concat(local.lb_external_subnets, [""]), count.index)
-  enable_resource_name_dns_a_record_on_launch    = var.lb_external_subnet_enable_resource_name_dns_a_record_on_launch
-  private_dns_hostname_type_on_launch            = var.private_dns_hostname_type_on_launch
-  vpc_id                                         = local.vpc_id
+  availability_zone                           = length(regexall("^[a-z]{2}-", element(local.azs, count.index))) > 0 ? element(local.azs, count.index) : null
+  availability_zone_id                        = length(regexall("^[a-z]{2}-", element(local.azs, count.index))) == 0 ? element(local.azs, count.index) : null
+  cidr_block                                  = element(concat(local.lb_external_subnets, [""]), count.index)
+  enable_resource_name_dns_a_record_on_launch = var.lb_external_subnet_enable_resource_name_dns_a_record_on_launch
+  private_dns_hostname_type_on_launch         = var.private_dns_hostname_type_on_launch
+  vpc_id                                      = local.vpc_id
 
   tags = merge(
     {
@@ -26,16 +26,13 @@ resource "aws_subnet" "lb_external" {
 }
 
 resource "aws_route_table" "lb_external" {
-  count = local.create_lb_external_route_table ? local.nat_gateway_count : 0
+  count = local.create_lb_external_route_table ? 1 : 0
 
   vpc_id = local.vpc_id
 
   tags = merge(
     {
-      "Name" = var.single_nat_gateway ? "${var.name}-${var.lb_external_subnet_suffix}" : format(
-        "${var.name}-${var.lb_external_subnet_suffix}-%s",
-        element(local.azs, count.index),
-      )
+      "Name" = "${var.name}-${var.lb_external_subnet_suffix}"
     },
     var.tags,
     var.lb_external_route_table_tags,
@@ -45,7 +42,7 @@ resource "aws_route_table" "lb_external" {
 resource "aws_route_table_association" "lb_external" {
   count = local.create_lb_external_subnets ? local.len_lb_external_subnets : 0
 
-  subnet_id      = element(aws_subnet.lb_external[*].id, count.index)
+  subnet_id = element(aws_subnet.lb_external[*].id, count.index)
   route_table_id = element(
     coalescelist(aws_route_table.lb_external[*].id, aws_route_table.public[*].id),
     local.create_lb_external_route_table ? var.single_nat_gateway ? 0 : count.index : count.index,
